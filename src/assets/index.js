222b7096b01c1925fe13177fb8f69f240b46154f
omersarisoy <81584800+omersarisoy@users.noreply.github.com>
2023-06-19 00:10:16 +0300

    last check

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import mui from "./tech/mui.png";
+import styledComponent from "./tech/styled-components.png";
+import ant from "./tech/ant-design.png";
+import bitbucket from "./tech/bitbucket.png";
+import sass from "./tech/sass.png";
+
+import asnak from "./company/asnak.png";
+
+import asnakv1 from "./asnakv1.gif";
+import asnakv2 from "./asnakv2.gif";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  sass,
+  ant,
+  styledComponent,
+  bitbucket,
+  mui,
+  asnakv1,
+  asnakv2,
+  asnak
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Strong understanding of web technologies including HTML5, CSS3, and JavaScript. <br />
+        2+ years of professional experience in Front-end Development. <br />
+        Experience in responsive design, cross-browser compatibility, and SEO optimization. <br />
+        Strong problem-solving skills and ability to work well in a team. <br />
+        A passion for staying up to date with the latest industry trends and technologies. <br />
+        Interested in writing new codes, skilled at developing complex solutions, possessing strong creative thinking skills,
+        high energy and integrity. <br />
+        Have the ability to create algorithms effectively, interact positively and communicate appropriately with team
+        members. <br />
+        Quick to grasp new technologies and concepts to develop innovative and creative solutions to problems. <br />
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
